4a3fa5ffaed43dca871a4cbc29b7846cd604caf3
fabagile <fabagile@outlook.fr>
2023-09-18 18:36:21 +0200

    erase history

--- src/app/components/page/Game_.jsx
+++ src/app/components/page/Game_.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import Board from "./Game.Board_";
-import History from "./Game.History";
+
 // import { useTranslation } from 'react-i18next'
 
 export default function Game({ t }) {
@@ -21,19 +21,12 @@ export default function Game({ t }) {
   return (
     <div className="my-5 mx-auto ">
       <Container className=" w-50">
-        <Row>
-          <Col xs={12} lg={6}>
-            <Board
-              xIsNext={xIsNext}
-              squares={currentSquares}
-              onPlay={handlePlay}
-              t={t}
-            />
-          </Col>
-          <Col xs={12} lg={6}>
-            <History setCurrentMove={setCurrentMove} history={history} t={t} />
-          </Col>
-        </Row>
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          t={t}
+        />        
       </Container>
     </div>
   );
