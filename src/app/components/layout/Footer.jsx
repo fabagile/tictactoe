3c976e63c36215b5acad06d2a0061cb142f1fe7d
fabagile <fabagile@outlook.fr>
2023-09-19 11:03:28 +0200

    footer mior fix

--- src/app/components/layout/Footer.jsx
+++ src/app/components/layout/Footer.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const Footer = ({ bgColor }) => {
-  const {t} = useTranslation()
-  const ghroute = 'https://github.com/fabagile'
-  return <footer className={`p-2 text-center bg-${bgColor}`} ><a href={`${ghroute}/tictactoe`} rel="noreferrer" target="_blank" title={t('sourceCode')} >tictactoe</a> <a href={ghroute} rel="noreferrer" target="_blank" title={t('siteAuthor')} > @fabagile</a> &copy; 2023</footer>;
+  const { t } = useTranslation();
+  const ghroute = "https://github.com/fabagile";
+  return (
+    <footer className={`p-2 text-center bg-${bgColor}`}>
+      <a
+        href={`${ghroute}/tictactoe`}
+        rel="noreferrer"
+        target="_blank"
+        title={t("sourceCode")}
+      >
+        tictactoe
+      </a>{" "}
+      <a
+        href={ghroute}
+        rel="noreferrer"
+        target="_blank"
+        title={t("siteAuthor")}
+      >
+        {" "}
+        @fabagile
+      </a>{" "}
+      &copy; 2023
+    </footer>
+  );
 };
 
 export default Footer;
