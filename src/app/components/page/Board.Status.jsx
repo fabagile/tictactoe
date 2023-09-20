926339887ad253e26c8116d99668016e60518b5d
fabagile <fabagile@outlook.fr>
2023-09-20 08:44:16 +0200

    winner status

--- src/app/components/page/Board.Status.jsx
+++ src/app/components/page/Board.Status.jsx
@@ -3,10 +3,10 @@ import { calculateWinner } from '../../utils/board.funcs'
 const Status = ({ squares, t, xIsNext }) => {
   const winner = calculateWinner(squares)
   const status = winner
-    ? `${t('status.winner')}: ${winner}`
-    : `${t('status.nextPlayer')}: ${xIsNext ? 'X' : 'O'}`
+    ? <span className='text-success' >{t('status.winner')}: <strong>{winner}</strong></span>
+    : <span className='' >{t('status.nextPlayer')}: {xIsNext ? 'X' : 'O'}</span>
 
-  return <div className='board__status'>{status}</div>
+  return <div className='board_status' >{status}</div>
 }
 
 export default Status
