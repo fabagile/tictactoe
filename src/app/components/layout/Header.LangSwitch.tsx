984c617b577aed93941bb98fdfd50c5893cf81b6
fabagile <fabagile@outlook.fr>
2023-09-22 07:31:24 +0200

    cursive fonts; switch lang flag

--- src/app/components/layout/Header.LangSwitch.tsx
+++ src/app/components/layout/Header.LangSwitch.tsx
@@ -1,18 +1,22 @@
 import i18next from 'i18next'
 import { languages } from '../../../utils/languages'
 import ReactCountryFlag from 'react-country-flag'
+import { useState } from 'react'
 
-export default function LangSwitch ({ currentLang }: {currentLang:any}) {
+export default function LangSwitch({ currentLang }: { currentLang: any }) {
+  const flagStyle = (code: any) => ({
+    fontSize: code === currentLang ? '2.0em' : '1.5em',
+    order: code !== currentLang ? 1 : 0,
+    filter: code !== currentLang ? "grayscale(50%)" : "grayscale(0)"
+  })
   return (
     <>
       {languages.map(({ code, name, countryCode }, i) => (
         <ReactCountryFlag
           key={i}
           onClick={() => i18next.changeLanguage(code)}
-          style={{
-            fontSize: code === currentLang ? '2.0em' : '1.5em'
-          }}
-          className='me-2'
+          style={flagStyle(code)}
+          className='me-2 '
           countryCode={countryCode}
           svg
         />
